7f9a3e6cff76397a893a072ec550ad28cfd8424d
Yahor Litavar <yahor.litavar@softnetix.io>
2023-06-12 17:38:34 +0300

    feat: list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const directoryUrl = '/files';
+
 const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const src = path.join(__dirname, directoryUrl);
+
+    fs.readdir(src, (err, files) => {
+        if (err) throw new Error('FS operation failed');
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
 };
 
 await list();
