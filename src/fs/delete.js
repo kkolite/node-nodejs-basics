3eaad08c4daa2db609edf8201598c6593c459c27
Yahor Litavar <yahor.litavar@softnetix.io>
2023-06-12 17:20:57 +0300

    feat: delete and read

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path'
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const src = path.join(__dirname, "files/fileToRead.txt");
+
+    fs.unlink(src, function (err) {
+        if (err) throw new Error('FS operation failed');
+        console.log('File deleted!');
+    });
 };
 
 await remove();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    try {
+        const src = path.join(__dirname, "files/fileToRead.txt");
+        const stream = fs.createReadStream(src);
+
+        let data = '';
+
+        stream.on('data', chunk => data += chunk);
+        stream.on('end', () => console.log(data));
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
