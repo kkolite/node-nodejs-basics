52be59f154ac580b78b596185437d28e18b4a164
Yahor Litavar <yahor.litavar@softnetix.io>
2023-06-12 17:26:52 +0300

    feat: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const text = 'I am fresh and young';
+const fileUrl = 'files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const src = path.join(__dirname, fileUrl);
+
+    fs.readFile(src, (err, data) => {
+        if (!err && data) throw new Error('FS operation failed');
+    })
+
+    fs.appendFile(src, text, function (err) {
+        if (err) throw new Error('FS operation failed');
+        console.log('Saved!');
+    });
 };
 
 await create();
