1aab13518e59c44b4b2248de721cd96c1223d998
Yahor Litavar <yahor.litavar@softnetix.io>
2023-06-12 17:32:25 +0300

    feat: rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const fileWrongUrl = 'files/wrongFilename.txt';
+const fileUrl = 'files/properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const src = path.join(__dirname, fileUrl);
+    const srcWrong = path.join(__dirname, fileWrongUrl);
+
+    fs.readFile(src, (err, data) => {
+        if (!err && data) throw new Error('FS operation failed');
+    })
+
+    fs.readFile(srcWrong, (err) => {
+        if (err) throw new Error('FS operation failed');
+    })
+
+    fs.rename(srcWrong, src, function (err) {
+        if (err) throw new Error('FS operation failed');
+        console.log('File Renamed!');
+    });
 };
 
 await rename();
