dbc6131ee2c638c5fbd66d1b95bb676fa18dfa29
Yahor Litavar <yahor.litavar@softnetix.io>
2023-06-14 15:31:52 +0300

    feat: calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,33 @@
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createHash } from 'node:crypto';
+
+const fileSrc = 'files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const createSHA = (data) => createHash('sha256').update(data).digest('hex');
+
+    const logSHA = (data) => {
+        const result = createSHA(data);
+        console.log(result);
+    }
+
+    try {
+        const src = path.join(__dirname, fileSrc);
+        const stream = fs.createReadStream(src);
+
+        let data = '';
+
+        stream.on('data', chunk => data += chunk);
+        stream.on('end', () => logSHA(data));
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await calculateHash();
