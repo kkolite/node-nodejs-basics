b0b65acea81ea014fd7c914b8067fc786a558a32
Yahor Litavar <yahor.litavar@softnetix.io>
2023-06-14 15:22:25 +0300

    feat: module

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,17 +1,16 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path, { dirname } from 'path';
+import { release, version } from 'os';
+import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+import './files/c.js';
+
+const { createServerHttp } = createServer;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const random = Math.random();
 
-let unknownObject;
-
-if (random > 0.5) {
-    unknownObject = require('./files/a.json');
-} else {
-    unknownObject = require('./files/b.json');
-}
+const unknownObject = import(random > 0.5 ? './files/a.json' : './files/b.json', {assert: {type: 'json'}});
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
@@ -20,11 +19,11 @@ console.log(`Path segment separator is "${path.sep}"`);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-const myServer = createServerHttp((_, res) => {
+const myServer = createServer((_, res) => {
     res.end('Request accepted');
 });
 
-const PORT = 3000;
+const PORT = 8000;
 
 console.log(unknownObject);
 
@@ -33,7 +32,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
