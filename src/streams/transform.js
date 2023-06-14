ba3d1427eba6bf0e5319d561b7190d5e4d442a8f
Yahor Litavar <yahor.litavar@softnetix.io>
2023-06-14 17:54:58 +0300

    feat: transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
-const transform = async () => {
-    // Write your code here 
-};
+import stream from 'stream';
+
+const { Transform } = stream;
+const { stdin, stdout } = process;
+
+function transform() {
+    const reverseStream = new Transform({
+        transform (data, encoding, callback) {
+            const reversedData = data.toString().split("").reverse().join("");
+            this.push(reversedData);
+            callback();
+        }
+    });
+
+    stdin.pipe(reverseStream).pipe(stdout);
+}
 
 await transform();
